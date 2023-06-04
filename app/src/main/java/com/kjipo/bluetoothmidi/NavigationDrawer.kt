b2a842f26d366de8b018589689d772939000d54b
Tore Brede <tore.brede@gmail.com>
2023-06-04 18:39:42 +0200

    Removing OptIn annotation that is no longer needed

--- app/src/main/java/com/kjipo/bluetoothmidi/NavigationDrawer.kt
+++ app/src/main/java/com/kjipo/bluetoothmidi/NavigationDrawer.kt
@@ -1,6 +1,5 @@
 package com.kjipo.bluetoothmidi
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
@@ -8,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavigationDrawer(
     navigateToMidiDevices: () -> Unit,
